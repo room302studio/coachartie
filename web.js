2f568cf24f04138372afef5f3e17382d7a8c059d
EJ Fox <ejfox@ejfox.com>
2023-07-03 15:31:31 -0400

    feat: add basic express interface to log file

---
+++ web.js
@@ -0,0 +1,41 @@
+/* an extraordinarily basic express web interface that provides access to the artie.log file */
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  // res.sendFile(path.join(__dirname, 'artie.log'));
+  /* add a basic web page skeleton with tachyons and the contents of the log file in a div */
+  fs.readFile(path.join(__dirname, 'artie.log'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Something went wrong');
+    } else {
+      res.send(`
+        <html>
+          <head>
+            <title>Artie Log</title>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tachyons/4.11.1/tachyons.min.css">
+          </head>
+          <body class="pa5 sans-serif">
+            <h1>Artie Log</h1>
+            <div class="mw9 center ph3-ns">
+              <div class="cf ph2-ns">
+                <div class="fl w-100 w-50-ns pa2">
+                    <pre>${data}</pre>
+                </div>
+              </div>
+            </div>
+          </body>
+        </html>
+      `);
+    }
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
