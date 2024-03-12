b62d11c4d17168b5acd5bccee0b630b097459bef
EJ Fox <ejfox@ejfox.com>
2024-03-11 22:45:10 -0400

    refactor: maybe there aren't Ids sometimes

--- api.js
+++ api.js
@@ -223,7 +223,7 @@ async function processMissiveRequest(body) {
   // check for any attachments in ANY of the conversation messages also
   // and if there are any memories of them, add them to the formattedMessages array
   conversationMessages.forEach((message) => {
-    logger.info(`Checking message ${message.id} for attachments`);
+    logger.info(`Checking message ${message?.id} for attachments`);
     const attachments = message.attachment;
 
     // log the keys available in message
@@ -232,7 +232,7 @@ async function processMissiveRequest(body) {
 
     if (attachments) {
       attachments.forEach(async (attachment) => {
-        const resourceId = attachment.id;
+        const resourceId = attachment?.id;
         logger.info(`Checking for memories of resource ${resourceId}`);
         const isInMemory = await hasRecentMemoryOfResource(resourceId); // check if we have ANY memory of this resource
         if (isInMemory) {
