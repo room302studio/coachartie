0aca1619d37f5ecc22912df08b3f21d2b0ce3c14
EJ Fox <ejfox@ejfox.com>
2024-03-17 19:00:56 -0400

    refactor: show available capabilities in player

--- capability-player.js
+++ capability-player.js
@@ -1,12 +1,37 @@
 const readline = require('readline');
 const { processCapability } = require('./src/chain');
 const { destructureArgs } = require('./helpers');
+const capabilityManifest = require('./capabilities/_manifest.json');
 
 // Create readline interface for command line input
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+
+function displayCapabilities() {
+  console.log('Available capabilities and their methods:');
+  Object.entries(capabilityManifest).forEach(([capability, methods]) => {
+    console.log(`\nCapability: ${capability}`);
+    methods.forEach(method => {
+      console.log(`  Method: ${method.name}`);
+      console.log(`    Description: ${method.description}`);
+      if (method.parameters) {
+        console.log('    Parameters:');
+        method.parameters.forEach(param => {
+          console.log(`      - ${param.name}: ${param.description}`);
+        });
+      }
+      if (method.exceptions) {
+        console.log('    Exceptions:');
+        method.exceptions.forEach(exception => {
+          console.log(`      - ${exception}`);
+        });
+      }
+    });
+  });
+  console.log('\nType the capability and method you want to use in the format: capability:methodName(args)');
+}
 
 // Function to ask user for input
 function askQuestion(query) {
@@ -33,11 +58,12 @@ async function processInputAsMessage(input) {
   const lastMessage = messages[messages.length - 1];
   console.log('Capability Response:', lastMessage.content);
 }
-
 // Main function to run the CLI
 async function main() {
   console.log('Capability Player CLI');
   console.log('Type "exit" to quit.');
+
+  displayCapabilities();
 
   while (true) {
     const input = await askQuestion('Enter capability and method (format: capabilitySlug:methodName(args)): ');
