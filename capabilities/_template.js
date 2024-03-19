3ab0f58db5983cb755ead423094ceb37aa710903
Curran <curran.kelleher@gmail.com>
2024-03-19 13:54:44 -0400

    Add template

---
+++ capabilities/_template.js
@@ -0,0 +1,42 @@
+const axios = require("axios");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const { destructureArgs } = require("../helpers");
+
+async function handleCapabilityMethod(method, args) {
+  const [arg1] = destructureArgs(args);
+
+  if (method === "askWolframAlpha") {
+    return askWolframAlpha(arg1);
+  } else {
+    throw new Error(
+      `Method ${method} not supported by Wolfram Alpha capability.`
+    );
+  }
+}
+
+/**
+ * @async
+ * @function askWolframAlpha
+ * @param {string} question - The question to ask Wolfram Alpha.
+ * @returns {Promise<string>} The answer from Wolfram Alpha, or an error message if an error occurred.
+ */
+async function askWolframAlpha(question) {
+  const wolframAppId = process.env.WOLFRAM_APP_ID;
+  // const [question] = destructureArgs(args);
+
+  const encodedQuestion = encodeURIComponent(question);
+  const wolframUrl = `https://www.wolframalpha.com/api/v1/llm-api?input=${encodedQuestion}&appid=${wolframAppId}`;
+
+  try {
+    const response = await axios.get(wolframUrl);
+    return response.data;
+  } catch (error) {
+    throw new Error(`Error occurred while contacting Wolfram Alpha: ${error}`);
+  }
+}
+
+module.exports = {
+  handleCapabilityMethod,
+};
