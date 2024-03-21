2e3e703feb81355b6a99d612d458fec48273d17b
EJ Fox <ejfox@ejfox.com>
2024-03-21 01:21:12 -0400

    feat: crete new ingest capability file

---
+++ capabilities/ingest.js
@@ -0,0 +1,35 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const axios = require("axios");
+
+const { destructureArgs } = require("../helpers");
+
+async function handleCapabilityMethod(method, args) {
+  const [arg1] = destructureArgs(args);
+
+  if (method === "deepDocumentIngest") {
+    return await deepDocumentIngest(arg1);
+  } else {
+    throw new Error(`Method ${method} not supported by this capability.`);
+  }
+}
+
+// This jsdoc documentation is parsed and passed to the robot capabilities through the manifest
+/**
+ * @async
+ * @function deepDocumentIngest
+ * @param {string} url - The URL to make an external request to.
+ * @returns {Promise<string>} The response from the external API, or an error message if an error occurred.
+ */
+async function deepDocumentIngest(url) {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    throw new Error(`Error occurred while making external request: ${error}`);
+  }
+}
+
+module.exports = {
+  handleCapabilityMethod,
+};
